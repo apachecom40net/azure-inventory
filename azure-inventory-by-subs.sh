90da7c49e56274c1f10a419d66092c1aa707d4ec
apachecom40net <apachecom40net@gmail.com>
2025-07-24 14:08:45 -0400

    repo updated

--- azure-inventory-by-subs.sh
+++ azure-inventory-by-subs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source ~/azure-inventory/.venv/bin/activate
-exec python ~/azure-inventory/azure-inventory.py "$@"
+source /usr/local/bin/azure-inventory/.venv/bin/activate
+exec python /usr/local/bin/azure-inventory.py "$@"
