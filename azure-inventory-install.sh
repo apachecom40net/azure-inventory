0589dac601900113bbd717c192e83de1e56db5bc
apachecom40net <apachecom40net@gmail.com>
2025-07-24 13:45:26 -0400

    bash file updated

--- azure-inventory-install.sh
+++ azure-inventory-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# install azure cli $$ Azure Resource Graph CLI extensions 
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az extension add --name resource-graph
+# # install azure cli $$ Azure Resource Graph CLI extensions 
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# az extension add --name resource-graph
 
-# clone this repo in /usr/local/bin
+# # clone this repo in /usr/local/bin
+
+git clone https://raw.githubusercontent.com/apachecom40net/azure-inventory /usr/local/bin/azure-inventory
